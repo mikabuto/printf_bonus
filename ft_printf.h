b0a47baf7e73e8cb82da953a37ccc915aa3aa573
mikabuto <92152153+mikabuto@users.noreply.github.com>
2022-01-04 01:57:14 +0300

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -31,9 +31,17 @@ int		cspdiuxX(char c);
 int		increase_pos(int *a);
 int		eval_prc_wdt(int *a, int *b, const char *format, int pos);
 int		ft_strlen(char *str);
-void	ft_fill_sp(t_print *tab, int len);
-int		ft_print_char(t_print *tab, int pos);
-int		ft_print_str(t_print *tab, int pos);
+void	ft_fill(t_print *tab, int len, char c);
+int		ft_print_char(t_print *tab);
+int		ft_print_str(t_print *tab);
 int		ft_min(int a, int b);
+void	clear_tab(t_print *tab);
+int		ft_putneghex(t_print *tab, int shift);
+void	ft_swap(char *a, char *b);
+int		ft_num_len(long long int n);
+int		ft_power(int a);
+int		ft_putnbr(t_print *tab);
+void	ft_putchar(char c, t_print *tab);
+int		ft_putstr(char *str, t_print *tab);
 
 # endif
